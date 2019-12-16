c1be614ad78a110f6a4707d1234ea5c6a119246e
vpbobade <51505151+vpbobade@users.noreply.github.com>
2019-12-16 20:11:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
 
-apt-get update && \
-apt-get install -y \
-    net-tools \
-    tree \
-    vim && \
-    yum \
-    sudo \
-rm -rf /var/lib/apt/lists/* && apt-get clean && apt-get purge
