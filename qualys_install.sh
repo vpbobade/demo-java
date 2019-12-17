492074fafa362fa419a734582c47e058407eee06
vpbobade <51505151+vpbobade@users.noreply.github.com>
2019-12-17 19:30:45 +0530

    Update qualys_install.sh

--- qualys_install.sh
+++ qualys_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Installing Qualys container sensor..."
+echo "Installing Qualys container sensor on remote docker host..."
 
-#sudo tar -xvf QualysContainerSensor.tar.xz
-sudo curl https://atmet8vb.s3.amazonaws.com/QualysContainerSensor.tar.xz && sudo tar -xf QualysContainerSensor.tar.xz"
-sudo mkdir -p /usr/local/qualys/sensor/data
-sudo ./installsensor.sh ActivationId=26ed0e3d-cffe-4c6b-9e79-bfaac22f1557 CustomerId=b34ba2b7-acfa-6ffc-83e6-7eff4061769f Storage=/usr/local/qualys/sensor/data -s -r
+sh 'ssh -t -t ec2-user@172.31.81.65 -o StrictHostKeyChecking=no'
+sh "sudo curl https://atmet8vb.s3.amazonaws.com/QualysContainerSensor.tar.xz && sudo tar -xf QualysContainerSensor.tar.xz"
+sh "sudo mkdir -p /usr/local/qualys/sensor/data"
+sh "sudo ./installsensor.sh ActivationId=26ed0e3d-cffe-4c6b-9e79-bfaac22f1557 CustomerId=b34ba2b7-acfa-6ffc-83e6-7eff4061769f Storage=/usr/local/qualys/sensor/data -s -r"	
